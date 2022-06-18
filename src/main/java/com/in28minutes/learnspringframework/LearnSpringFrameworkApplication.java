88a70ac0642e7c4ee15339217529c4032fee5965
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-06-18 23:40:30 +0800

    committing component scan annotation for understanding what is happening in the background

--- src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/LearnSpringFrameworkApplication.java
@@ -3,12 +3,12 @@ package com.in28minutes.learnspringframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
-import com.in28minutes.learnspringframework.game.MarioGame;
-import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 @SpringBootApplication
+@ComponentScan("com.in28minutes.learnspringframework")
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
